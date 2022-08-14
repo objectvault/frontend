0fd82b641542e383c477dcf4c643851259312347
Paulo Ferreira <pf@sourcenotes.org>
2022-08-14 22:52:05 +0100

    Added preprocessor helper.

---
+++ src/dev-utils.ts
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the ObjectVault Project.
+ * Copyright (C) 2020-2022 Paulo Ferreira <vault at sourcenotes.org>
+ *
+ * This work is published under the GNU AGPLv3.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const utilities: any = {
+  throwMessage: (m: string) => { throw m; }
+}
+
+export default utilities;
